60c6ee03a1cefdde0cc8e25f152922afb6c09915
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-26 20:39:30 +0200

    last commit, waiting for tomorrow correction

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-const slider = document.querySelector('.container-img');
+const slider = document.querySelector('.img-wrapper');
 const thumbsSlider = document.querySelector('.container-thumbs');
+
 
 const images = [
     {
-        title   : "svezia",
+        title   : "Ciberpunk Oriental City",
         caption : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur.",
-        rif     : "sweden.jpg"
+        rif     : "cyberpunk.jpg"
     },
     
     {
@@ -17,7 +18,7 @@ const images = [
     {
         title   : "Colombia",
         caption : "Pablo's Home.",
-        rif     : "pablo.jpg"
+        rif     : "mountain-lake.jpg"
     },
 
     {
@@ -39,26 +40,33 @@ console.table(images);
 //STAMPO LE IMMAGINI NEL DOM
 
 images.forEach(el  => {
-    slider.innerHTML += ` <img class="item" src="img/${el.rif}" alt="${el.title}"> `;
-    thumbsSlider.innerHTML += ` <img src="img/${el.rif}" alt="${el.title}"> `;
+    slider.innerHTML += `
+                            <h2 class="info-text hide">${el.title}</h2> <span class="info-text hide">${el.caption}</span>
+    <img class="item" src="img/${el.rif}" alt="${el.title}"> `;
+    thumbsSlider.innerHTML += ` <img class="thumb" src="img/${el.rif}" alt="${el.title}"> `;
 });
 
 //CREO UN CONTATORE PER LE IMMAGINI, LE AGGIUNGO AD UNA COLLECTION ED ASSEGNO LA CLASSE ACTIVE AL PRIMO ELEMENTO
 let counterImg = 0;
 const item = document.getElementsByClassName('item');
 item[counterImg].classList.add('active');
+const thumb = document.getElementsByClassName('thumb');
+thumb[counterImg].classList.add('active-thumb');
 
-
+const information = document.getElementsByClassName('info-text');
+information[counterImg].classList.add('active-info');
+console.log(information)
 //NAVIGAZIONE
-
+automaticSlider();
 //bottoni
 const next = document.querySelector('.right');
 const prev = document.querySelector('.left');
-let nav;
+
 next.addEventListener('click', function(){
     console.log('AVANTI')
     nav = 0;
     nextPrev();
+    clearInterval(autoplay);
 })
 
 
@@ -66,20 +74,47 @@ prev.addEventListener('click', function(){
     console.log('DIETRO')
     nav = 1;
     nextPrev();
+    clearInterval(autoplay);
 })
 
+
+
+
+
+
+//creo una variabile booleana per la funzione di navigazione
+let nav;
 function nextPrev(){
     if(nav === 0){
         item[counterImg].classList.remove('active');
+        thumb[counterImg].classList.remove('active-thumb');
+        
         counterImg++;
         if(counterImg === images.length) counterImg = 0;
         item[counterImg].classList.add('active');
+        thumb[counterImg].classList.add('active-thumb');
 
     } else{
         item[counterImg].classList.remove('active');
+        thumb[counterImg].classList.remove('active-thumb');
+
+        
         counterImg--;
         if(counterImg < 0) counterImg = images.length - 1;
-        item[counterImg].classList.add('active');
 
+        
+        thumb[counterImg].classList.add('active-thumb');
     }
+}
+
+function automaticSlider(){
+    
+    const autoplay = setInterval(function(){
+        item[counterImg].classList.remove('active');
+        thumb[counterImg].classList.remove('active-thumb');
+        counterImg++;
+        if(counterImg === images.length) counterImg = 0;
+        item[counterImg].classList.add('active');
+        thumb[counterImg].classList.add('active-thumb');
+    }, 1000);
 }
